452216d2980fe7f7c553ac8d93944645bc7ec9bd
chronoreaper <chris.dd.29@gmail.com>
2020-02-07 13:52:44 -0500

    Initial commit

---
+++ AxeAccessibilityDriver/AxeAccessibilityDriver/Axe/IAccessibilityChecker.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="IAccessibilityChecker.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace SeleniumPerfXML.Axe
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using OpenQA.Selenium;
+
+    /// <summary>
+    /// The Accessibility interface.
+    /// </summary>
+    public interface IAccessibilityChecker
+    {
+        /// <summary>
+        /// Gets or sets the webdriver variable.
+        /// </summary>
+        public IWebDriver WebDriver { get; set; }
+    }
+}
