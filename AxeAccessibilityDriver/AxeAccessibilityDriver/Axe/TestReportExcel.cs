f4f8b1b1b2118c9063f5545d12f654bea33fe092
chronoreaper <chris.dd.29@gmail.com>
2020-02-07 16:23:21 -0500

    Create TestReportExcel.cs

---
+++ AxeAccessibilityDriver/AxeAccessibilityDriver/Axe/TestReportExcel.cs
@@ -0,0 +1,29 @@
+﻿using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AxeAccessibilityDriver.Axe
+{
+    class TestReportExcel
+    {
+        Dictionary<string,Dictionary<ColumnNames, string>> ExcelData;
+
+        string ProjectName;
+        string ProjectUrl;
+        string Date;
+
+        public void WriteToExcel()
+        {
+            IWorkbook workbook = null;
+        }
+        public enum ColumnNames
+        {
+            Id,
+            Level,
+            Criteria,
+            Comments
+        }
+    }
+}
