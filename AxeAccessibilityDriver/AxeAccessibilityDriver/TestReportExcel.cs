6c59a69d5d2cc5ce6ec1040452efb115ba69db7b
chronoreaper <chris.dd.29@gmail.com>
2020-02-10 10:14:55 -0500

    Update TestReportExcel.cs

--- AxeAccessibilityDriver/AxeAccessibilityDriver/TestReportExcel.cs
+++ AxeAccessibilityDriver/AxeAccessibilityDriver/TestReportExcel.cs
@@ -10,13 +10,61 @@ namespace AxeAccessibilityDriver
 {
     public class TestReportExcel
     {
-        Dictionary<string,Dictionary<ColumnNames, string>> ExcelData;
+        /// <summary>
+        /// The data to write to the excel sheet
+        /// </summary>
+        public Dictionary<string, Dictionary<ColumnNames, string>> ExcelData;
 
         public string ProjectName;
         public string ProjectUrl;
         public string Date;
         public string fileLocation;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TestReportExcel"/> class.
+        /// Creates a new Excel report.
+        /// </summary>
+        public TestReportExcel()
+        {
+            this.ExcelData = new Dictionary<string, Dictionary<ColumnNames, string>>();
+        }
+
+        /// <summary>
+        /// The names of the Columns.
+        /// </summary>
+        public enum ColumnNames
+        {
+            /// <summary>
+            /// Id of the AODA.
+            /// </summary>
+            Id,
+
+            /// <summary>
+            /// Usualy AA or A.
+            /// </summary>
+            Level,
+
+            /// <summary>
+            /// Pass, Fail or N/A.
+            /// </summary>
+            Criteria,
+
+            /// <summary>
+            /// Any comments provided.
+            /// </summary>
+            Comments,
+        }
+
+        private enum Criteria
+        {
+            Pass,
+            Fail,
+            NA,
+        }
+
+        /// <summary>
+        /// Writes the aoda results to the excel file.
+        /// </summary>
         public void WriteToExcel()
         {
             IWorkbook workbook = null;
@@ -26,34 +74,34 @@ namespace AxeAccessibilityDriver
             string filePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\AODA_Template.xlsx";
             string resultFilePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + $"//AODA_Result_{DateTime.Now:MM_dd_yyyy_hh_mm_ss_tt}.xlsx";
 
-            using (FileStream expectedFS = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            using (FileStream templateFS = new FileStream(filePath, FileMode.Open, FileAccess.Read))
             {
-                using (FileStream fileStream = new FileStream(resultFilePath, FileMode.Create, FileAccess.ReadWrite))
-                {
-                    workbook = new XSSFWorkbook();
-                    sheet = workbook.CreateSheet();
-
-                    ICellStyle cellStyle = workbook.CreateCellStyle();
-                    cellStyle.FillForegroundColor = NPOI.HSSF.Util.HSSFColor.Yellow.Index;
-                    cellStyle.FillPattern = FillPattern.SolidForeground;
-
-                    IRow row = sheet.CreateRow(0);
-                    ICell cell = row.CreateCell(0);
-
-                    cell.CellStyle = cellStyle;
-                    cell.SetCellValue("Hi there");
-
-                    workbook.Write(fileStream);
-                }
+                workbook = new XSSFWorkbook(templateFS);
             }
-        }
 
-        public enum ColumnNames
-        {
-            Id,
-            Level,
-            Criteria,
-            Comments
+            // Define styles
+            ICellStyle passStyle = workbook.CreateCellStyle();
+            passStyle.FillForegroundColor = NPOI.HSSF.Util.HSSFColor.Green.Index;
+            passStyle.FillPattern = FillPattern.SolidForeground;
+
+            ICellStyle failStyle = workbook.CreateCellStyle();
+            failStyle.FillForegroundColor = NPOI.HSSF.Util.HSSFColor.Red.Index;
+            failStyle.FillPattern = FillPattern.SolidForeground;
+
+            // get the sheet to modify.
+            sheet = workbook.GetSheet("WCAG 2.0 Compliance Checklist");
+
+            IRow row = sheet.GetRow(13 - 1);
+            ICell cell = row.GetCell(4 - 1);
+
+            cell.SetCellValue("Pass");
+
+            //write to output.
+            using (FileStream fileStream = new FileStream(resultFilePath, FileMode.Create, FileAccess.Write))
+            {
+                workbook.Write(fileStream);
+                workbook.Close();
+            }
         }
     }
 }
