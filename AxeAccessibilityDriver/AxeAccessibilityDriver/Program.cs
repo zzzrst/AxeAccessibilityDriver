b656fc823e626f559d6eae3ce3480ba0ed1b76f0
chronoreaper <chris.dd.29@gmail.com>
2020-02-11 09:20:17 -0500

    Revert "Delete Program.cs"
    This reverts commit 5e69936f4e6e01f822f9f2c9535e8235642bd9fc.

---
+++ AxeAccessibilityDriver/AxeAccessibilityDriver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AxeAccessibilityDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
