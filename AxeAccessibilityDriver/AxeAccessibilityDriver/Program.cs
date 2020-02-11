d07e57e3db2723c69d94231b03ff036ce6eebc77
chronoreaper <chris.dd.29@gmail.com>
2020-02-11 10:23:13 -0500

    Update Program.cs

--- AxeAccessibilityDriver/AxeAccessibilityDriver/Program.cs
+++ AxeAccessibilityDriver/AxeAccessibilityDriver/Program.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace AxeAccessibilityDriver
 {
-    class Program
+    using System;
+
+    /// <summary>
+    /// Not used.
+    /// </summary>
+    public class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Not used.
+        /// </summary>
+        /// <param name="args">arguments.</param>
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
