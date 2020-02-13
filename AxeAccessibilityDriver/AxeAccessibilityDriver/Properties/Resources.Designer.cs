da4eca77d2183556367cfc3185ad053a316a02a4
chronoreaper <chris.dd.29@gmail.com>
2020-02-13 12:00:21 -0500

    Update Resources.Designer.cs

--- AxeAccessibilityDriver/AxeAccessibilityDriver/Properties/Resources.Designer.cs
+++ AxeAccessibilityDriver/AxeAccessibilityDriver/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SeleniumPerfXML.Properties {
+namespace AxeAccessibilityDriver.Properties {
     using System;
     
     
